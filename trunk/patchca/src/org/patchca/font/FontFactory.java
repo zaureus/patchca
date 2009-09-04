0f5e547bd8721bb9a5af7cdd76341b548663e03d
leech.miranda@gmail.com <leech.miranda@gmail.com@9774e89a-76c4-11de-b058-93adefbabda0>
2009-09-04 10:22:54 +0000

    fix typo, change package names

--- trunk/patchca/src/org/patchca/font/FontFactory.java
+++ trunk/patchca/src/org/patchca/font/FontFactory.java
@@ -1,20 +1,20 @@
 /*
  * Copyright (c) 2009 Piotr Piastucki
  * 
- * This file is part of Patcha CAPTCHA library.
+ * This file is part of Patchca CAPTCHA library.
  * 
- *  Patcha is free software: you can redistribute it and/or modify
+ *  Patchca is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *  
- *  Patcha is distributed in the hope that it will be useful,
+ *  Patchca is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.
  *  
  *  You should have received a copy of the GNU Lesser General Public License
- *  along with Patcha. If not, see <http://www.gnu.org/licenses/>.
+ *  along with Patchca. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.patchca.font;
 
